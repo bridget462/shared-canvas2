efb4a6f30c7d8a96d702579f4eab068cf938489b
bridget462 <bridges.dev.create@gmail.com>
2020-10-11 17:41:09 +0900

    dwaring shared

--- public/sketch.js
+++ public/sketch.js
@@ -2,10 +2,30 @@ let socket;
 
 function setup() {
   createCanvas(800, 500);
+  background("black");
   socket = io.connect("http://localhost:3000");
+
+  socket.on("mouse", newDrawing);
 }
 
-function draw() {
-  background("black");
-  rect(100, 200, 75, 125);
+function newDrawing(data) {
+  noStroke();
+  fill(255);
+  ellipse(data.x, data.y, 36, 36);
 }
+
+function mouseDragged() {
+  console.log("sending", mouseX, mouseY);
+  let data = {
+    x: mouseX,
+    y: mouseY,
+  };
+
+  socket.emit("mouse", data);
+
+  noStroke();
+  fill(255);
+  ellipse(mouseX, mouseY, 36, 36);
+}
+
+function draw() {}
--- server.js
+++ server.js
@@ -9,6 +9,13 @@ app.use(express.static("./public"));
 
 function newConnection(socket) {
   console.log("new connection", socket.id);
+
+  socket.on("mouse", mouseMsg);
+
+  function mouseMsg(data) {
+    socket.broadcast.emit("mouse", data);
+    console.log(data);
+  }
 }
 
 io.sockets.on("connection", newConnection);
